7e4bef8b431958e1b26fef235d6396cf73eab971
Derrick Ngaha <ngahaderrick@gmail.com>
2026-03-23 10:19:46 +0100

    fix: cache dummy pour tests, ProductTest.php, mkdir writable dans CI

---
+++ tests/ProductTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests;
+
+use CodeIgniter\Test\CIUnitTestCase;
+
+/**
+ * Tests généraux sur le produit
+ */
+class ProductTest extends CIUnitTestCase
+{
+    public function testPlaceholder(): void
+    {
+        $this->assertTrue(true);
+    }
+}
